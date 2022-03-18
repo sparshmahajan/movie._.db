ccfcc3fdcf3dcbacb4b9fb3856f9cff279da86d3
sparshmahajan <sparshmahajan3@gmail.com>
2022-03-19 01:49:49 +0530

    backend data add

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,16 +1,24 @@
-import poster from '../../assets/images/poster.jpg';
+import defaultProps from 'react-slick/lib/default-props';
 import classes from './Card.module.css';
 
-const Card = () => {
+const Card = (props) => {
+
+    const media_type = props.children[1].media_type;
+    const title = media_type === "movie" ? props.children[1].title : props.children[1].name;
+    const poster_path = props.children[1].poster_path;
+    const vote_average = props.children[1].vote_average;
+    const release_date = media_type === "movie" ? props.children[1].release_date : props.children[1].first_air_date;
+    const language = props.children[1].original_language;
+
+
     return (
         <div className={classes.card}>
-            <img src={poster} alt="poster" className={classes.poster} />
+            <img src={"https://image.tmdb.org/t/p/original" + poster_path} alt="poster" className={classes.poster} />
             <ul className={classes.card_content}>
-                <li id={classes.title}>Attack On Titan</li>
-                <li>Year : 2016</li>
-                <li>Genre : Action, Drama, Fantasy</li>
-                <li>Director : Hayao Miyazaki</li>
-                <li> Ratings : 10</li>
+                <li id={classes.title}>{title}</li>
+                <li>Language : {language}</li>
+                <li>Release Date : {release_date}</li>
+                <li> Ratings : {vote_average}</li>
             </ul>
         </div>);
 };
--- src/components/UI/CardHolder.jsx
+++ src/components/UI/CardHolder.jsx
@@ -1,39 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import Card from "./Card";
+import axios from 'axios';
 import Carousel from "react-elastic-carousel";
 import classes from "./CardHolder.module.css";
-
 
 const breakPoints = [
     { width: 1, itemsToShow: 1 },
     { width: 350, itemsToShow: 2, itemsToScroll: 2 },
     { width: 550, itemsToShow: 3, itemsToScroll: 3 },
-    { width: 768, itemsToShow: 4, itemsToScroll: 4 },
-    { width: 900, itemsToShow: 5, itemsToScroll: 5 },
-    { width: 1200, itemsToShow: 6, itemsToScroll: 6 },
-    { width: 1400, itemsToShow: 8, itemsToScroll: 8 },
-    { width: 1600, itemsToShow: 10, itemsToScroll: 10 },
+    { width: 768, itemsToShow: 3, itemsToScroll: 3 },
+    { width: 900, itemsToShow: 4, itemsToScroll: 3 },
+    { width: 1200, itemsToShow: 5, itemsToScroll: 5 },
+    { width: 1400, itemsToShow: 7, itemsToScroll: 7 },
+    { width: 1600, itemsToShow: 9, itemsToScroll: 9 },
 ];
 
+let search_word = 'movie';
+
 const CardHolder = (props) => {
+
+    if (props.children === 'MOVIES') {
+        search_word = 'movie';
+    } else {
+        search_word = 'tv';
+    }
+
+    const url = "http://localhost:5000/tmdb/trending_" + search_word;
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get(url);
+            try {
+                setData(response.data);
+            } catch (error) {
+                console.log(error);
+                setData([]);
+            }
+        };
+        fetchData();
+    }, []);
 
     return (
         <React.Fragment>
             <h1 className={classes.title}>Trending {props.children} </h1>
             <Carousel breakPoints={breakPoints} >
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                {data.map((item) =>
+                    <Card key={item.id}> {item} </Card>
+                )}
             </Carousel>
         </React.Fragment>
     );
