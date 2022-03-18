545bb6ea097077a82f28e9ead5b0c0ee7820d3df
sparshmahajan <sparshmahajan3@gmail.com>
2022-03-18 23:30:49 +0530

    carousel fixed

--- src/components/UI/CardHolder.jsx
+++ src/components/UI/CardHolder.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Card from "./Card";
+import Carousel from "react-elastic-carousel";
 import classes from "./CardHolder.module.css";
 
+
+const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+    { width: 768, itemsToShow: 4, itemsToScroll: 4 },
+    { width: 1200, itemsToShow: 6, itemsToScroll: 6 },
+    { width: 1400, itemsToShow: 7, itemsToScroll: 7 },
+];
+
 const CardHolder = (props) => {
+
     return (
         <React.Fragment>
             <h1 className={classes.title}>Trending {props.children} </h1>
-            <div className={classes.card_holder}>
+            <Carousel breakPoints={breakPoints} >
                 <Card />
                 <Card />
                 <Card />
@@ -17,7 +28,10 @@ const CardHolder = (props) => {
                 <Card />
                 <Card />
                 <Card />
-            </div>
+                <Card />
+                <Card />
+                <Card />
+            </Carousel>
         </React.Fragment>
     );
 }
