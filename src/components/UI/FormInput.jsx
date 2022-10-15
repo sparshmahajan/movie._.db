8f20fbea8f2c1ecad47d496f5b20b7399a0b1b8c
Md Saquib Shakeel <saquib.shakeel.1866@gmail.com>
2022-10-16 03:39:25 +0530

    auth validation and styles

--- src/components/UI/FormInput.jsx
+++ src/components/UI/FormInput.jsx
@@ -1,21 +1,23 @@
-import classes from './FormInput.module.css';
+import classes from "./FormInput.module.css";
 
 const FormInput = (props) => {
-    return (
-        <div className={classes.form_input}>
-            <label className={classes.label}>{props.label}</label>
-            <input
-                className={classes.input}
-                type={props.type}
-                name={props.name}
-                value={props.value}
-                onChange={props.handleChange}
-                placeholder={props.label}
-                ref={props.Ref}
-                required
-            />
-        </div>
-    );
-}
+  return (
+    <div className={classes.form_input}>
+      <label className={classes.label}>{props.label}</label>
+      <input
+        className={classes.input}
+        type={props.type}
+        name={props.name}
+        value={props.value}
+        onChange={props.handleChange}
+        placeholder={props.label}
+        onBlur={props.handleBlur}
+        ref={props.Ref}
+        required
+      />
+      {props.hasError && <p className={classes.error}>{props.errorMessage}</p>}
+    </div>
+  );
+};
 
 export default FormInput;
--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -1,103 +1,141 @@
-import { Fragment, useRef } from "react";
+import { Fragment, useRef, useState } from "react";
 import FormInput from "../UI/FormInput";
 import CustomButton from "../UI/CustomButton";
 import classes from "./SignIn.module.css";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const SignUp = () => {
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [isConfirmPasswordValid, setIsConfirmPasswordValid] = useState(true);
 
-    const nameRef = useRef();
-    const emailRef = useRef();
-    const passwordRef = useRef();
-    const confirmPasswordRef = useRef();
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const emailValidity = () => {
+    const re = /\S+@\S+\.\S+/;
+    const result = re.test(emailRef.current.value);
+    setIsEmailValid(result);
+    return result;
+  };
 
-        if (passwordRef.current.value !== confirmPasswordRef.current.value) {
-            toast.error("Passwords do not match");
-            return;
+  const passwordValidity = () => {
+    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    const result = re.test(passwordRef.current.value);
+    setIsPasswordValid(result);
+    return result;
+  };
+
+  const confirmPasswordValidity = () => {
+    const result =
+      passwordRef.current.value === confirmPasswordRef.current.value;
+    setIsConfirmPasswordValid(result);
+    return result;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const validE = emailValidity();
+    const validP = passwordValidity();
+    const validCP = confirmPasswordValidity();
+
+    if (validE && validP && validCP) {
+      const body = {
+        name: nameRef.current.value,
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      };
+      const fetchData = async () => {
+        try {
+          const response = await axios.post(
+            "https://moviedb-backend-1.herokuapp.com/api/signup",
+            body
+          );
+          toast.success(response.data.message, {
+            position: "top-right",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+          });
+          setTimeout(() => navigate("/signin"), 2000);
+        } catch (error) {
+          console.log(error);
+          const error_msg = error.response.data.message;
+          toast.error(error_msg, {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+          });
         }
+      };
+      fetchData();
+    } else return;
+  };
 
-        const body = {
-            name: nameRef.current.value,
-            email: emailRef.current.value,
-            password: passwordRef.current.value
-        };
-        const fetchData = async () => {
-            try {
-                const response = await axios.post("https://moviedb-backend-1.herokuapp.com/api/signup", body);
-                toast.success(response.data.message, {
-                    position: "top-right",
-                    autoClose: 1000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true
-                });
-                setTimeout(() => navigate("/signin"), 2000);
-            } catch (error) {
-                console.log(error);
-                const error_msg = error.response.data.message;
-                toast.error(error_msg, {
-                    position: toast.POSITION.TOP_RIGHT,
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true
-                });
-            }
-        };
-        fetchData();
-    };
-
-    return (
-        <Fragment>
-            <div className={classes.center}>
-                <div className={classes.sign_in}>
-                    <h1>SIGN UP</h1>
-                    <form onSubmit={handleSubmit}>
-                        <FormInput
-                            name="displayName"
-                            type="text"
-                            label="Display Name"
-                            Ref={nameRef}
-                            required
-                        />
-                        <FormInput
-                            name="email"
-                            type="email"
-                            label="Email"
-                            Ref={emailRef}
-                            required
-                        />
-                        <FormInput
-                            name="password"
-                            type="password"
-                            label="Password"
-                            Ref={passwordRef}
-                            required
-                        />
-                        <FormInput
-                            name="confirmPassword"
-                            type="password"
-                            label="Confirm Password"
-                            Ref={confirmPasswordRef}
-                            required
-                        />
-                        <CustomButton type="submit">Sign up</CustomButton>
-                    </form>
-                    <p>Already have an account ? <Link to="/signin" className={classes.link}>Sign in</Link></p>
-                </div>
-            </div>
-            <ToastContainer />
-        </Fragment>
-    );
-}
+  return (
+    <Fragment>
+      <div className={classes.center}>
+        <div className={classes.sign_in}>
+          <h1>SIGN UP</h1>
+          <form className={classes.signInForm} onSubmit={handleSubmit}>
+            <FormInput
+              name="displayName"
+              type="text"
+              label="Display Name"
+              Ref={nameRef}
+              required
+            />
+            <FormInput
+              name="email"
+              type="email"
+              label="Email"
+              Ref={emailRef}
+              hasError={!isEmailValid}
+              errorMessage="Please enter a valid email address"
+              required
+            />
+            <FormInput
+              name="password"
+              type="password"
+              label="Password"
+              Ref={passwordRef}
+              hasError={!isPasswordValid}
+              errorMessage="Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter and one number"
+              required
+            />
+            <FormInput
+              name="confirmPassword"
+              type="password"
+              label="Confirm Password"
+              Ref={confirmPasswordRef}
+              handleBlur={confirmPasswordValidity}
+              hasError={!isConfirmPasswordValid}
+              errorMessage="Passwords do not match"
+              required
+            />
+            <CustomButton type="submit">Sign up</CustomButton>
+          </form>
+          <p>
+            Already have an account ?{" "}
+            <Link to="/signin" className={classes.link}>
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </div>
+      <ToastContainer />
+    </Fragment>
+  );
+};
 
 export default SignUp;
